95647956295b013fbbb5690cc3c2a557592642bf
Adrian Saffarnia <info@buildtoweb.com>
2024-12-01 16:23:55 +0100

    height fix

--- pages/advent-calendar/messages.js
+++ pages/advent-calendar/messages.js
@@ -1,92 +1,58 @@
 const messages = [
   [
-    "There is nothing in the world so irresistibly contagious as laughter and good humor.",
-    "Charles Dickens",
+    "Herzlich Willkommen zu diesen Adventkalender! Ich habe für dich diese kleine funktionale Website entwickelt wo du auf Funktion wie Beispielsweise diesen Adventkalender zugreifen kannst. Vielleicht wird im Laufe unserer Beziehung noch mehr dazukommen! \n Im Kalender werden dich ein paar nette Worte erwarten. Ich hoffe es ist nicht zu wenig für dich. Ich habe mir gedacht ich mache mal was anderes.",
   ],
-  [
-    "Christmas, children, is not a date. It is a state of mind.",
-    "Mary Ellen Chase",
-  ],
+  ["Christmas, children, is not a date. It is a state of mind."],
   [
     "Christmas waves a magic wand over this world, and behold, everything is softer and more beautiful.",
-    "Norman Vincent Peale",
   ],
   [
     "Blessed is the season which engages the whole world in a conspiracy of love.",
-    "Hamilton Wright Mabie",
   ],
   [
     "A lovely thing about Christmas is that it's compulsory, like a thunderstorm, and we all go through it together.",
-    "Garrison Keillor",
   ],
   [
     "Christmas is a necessity. There has to be at least one day of the year to remind us that we're here for something else besides ourselves.",
-    "Eric Sevareid",
   ],
   [
     "Christmas magic is silent. You don't hear it --- you feel it, you know it, you believe it.",
-    "Kevin Alan Milne",
   ],
   [
     "Never worry about the size of your Christmas tree. In the eyes of children, they are all thirty feet tall.",
-    "Larry Wilde",
   ],
-  [
-    "Peace on Earth will come to stay, when we live Christmas every day.",
-    "Helen Steiner Rice",
-  ],
-  [
-    "What kind of Christmas present would Jesus ask Santa for?",
-    "Salman Rushdie",
-  ],
+  ["Peace on Earth will come to stay, when we live Christmas every day."],
+  ["What kind of Christmas present would Jesus ask Santa for?"],
   [
     "Unless we make Christmas an occasion to share our blessings, all the snow in Alaska won't make it white.",
-    "Bing Crosby",
   ],
   [
     "Gifts of time and love are surely the basic ingredients of a truly merry Christmas.",
-    "Peg Bracken",
   ],
-  ["The real Santa Claus is at the mall.", "Lemony Snicket"],
+  ["The real Santa Claus is at the mall."],
   [
     "What if, doesn't come from a store. What if Christmas... perhaps... means a little bit more!",
-    "Dr. Seuss",
   ],
-  [
-    "It was often in small moments that significant things were revealed.",
-    "Lisa Kleypas",
-  ],
+  ["It was often in small moments that significant things were revealed."],
   [
     "I wish we could put up some of the Christmas spirit in jars and open a jar of it every month.",
-    "Harlan Miller",
   ],
   [
     "Christmas is not an external event at all, but a piece of one's home that one carries in one's heart.",
-    "Freya Stark",
   ],
   [
     "Christmas, my child, is love in action. Every time we love, every time we give, it's Christmas.",
-    "Dale Evans Rogers",
   ],
-  [
-    "Christmas is the keeping-place for memories of our innocence.",
-    "Joan Mills",
-  ],
+  ["Christmas is the keeping-place for memories of our innocence."],
   [
     "One can never receive enough love, nor, can they give enough. Love is the one thing we can never have enough of, nor, give too much.",
-    "Caedem Marquez",
   ],
   [
     "Do give books for Christmas. They're never fattening, seldom sinful, and permanently personal.",
-    "Lenore Hershey",
   ],
-  [
-    "Christmas works like glue, it keeps us all sticking together.",
-    "Rosie Thomas",
-  ],
+  ["Christmas works like glue, it keeps us all sticking together."],
   [
     "Christmas is a bridge. We need bridges as the river of time flows past. Today's Christmas should mean creating happy hours for tomorrow and reliving those of yesterday.",
-    "Gladys Taber",
   ],
-  ["MERRY CHRISTMAS", "hongkiat.com"],
+  ["MERRY CHRISTMAS"],
 ];
